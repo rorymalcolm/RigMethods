323e68d1d47990bab5d6ec42f8c8c96a3f62d268
Rory Malcolm <RoryMcKenzieMalcolm@gmail.com>
2016-09-27 13:07:57 +0100

    changes to sensors.adb

--- ProjectFiles/sensors.adb
+++ ProjectFiles/sensors.adb
@@ -1,7 +1,22 @@
 package body Sensors
 is
-State := Sensor_Type;
+State : Sensor_Type;
 
-begin;
-    Sensor_Type := Proceed;
-end;
+begin
+   Sensor_Type := Proceed;
+   procedure Write_Sensors(Value_1, Value_2, Value_3: in Sensor_Type)
+   is
+   begin
+      
+   end Writer_Sensors;
+   function Read_Sensor(Sensor_Index: in Sensor_Index_Type)
+   is
+   begin
+      
+   end Read_Sensor;
+   function Read_Sensor_Majority()
+   is
+   begin
+      
+   end Read_Sensor_Majority;
+end Sensors;
